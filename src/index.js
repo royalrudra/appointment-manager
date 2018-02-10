9bfa54e6c03ccf48bc7dcc22a8c6782c2ef52e84
Rudra Singh <royal4rudra@gmail.com>
2018-02-10 14:27:15 +0545

    init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import registerServiceWorker from './registerServiceWorker';
+import 'antd/dist/antd.min.css';
+import './index.css';
+import Router from './component/Router';
+
+ReactDOM.render(<Router />, document.getElementById('root'));
+// registerServiceWorker();
